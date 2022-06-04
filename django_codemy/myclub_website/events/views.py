5e77bf869948617c0e98a7ace736934f280a37f3
Vishwa Praveen <vishwapraveen4618@gmail.com>
2022-06-04 09:59:48 +0530

    new django

--- django_codemy/myclub_website/events/views.py
+++ django_codemy/myclub_website/events/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
 
 # Create your views here.
+def home(request, year, month):
+    name='Vishwa'
+    month=month.title() #convert to  uppercase
+    # convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number=int(month_number)
+
+    return render(request,
+    'home.html',{
+        'name':name,
+        'year':year,
+        'month':month,
+        'month_number':month_number
+        })
